4d7605778671e9eef81a8e43a99cbed29726878e
LoicMartin34 <loic.martin34670@gmail.com>
2020-02-23 15:18:21 +0100

    testStats
    Ajout histogramme neurones + discrétisation

--- testStats.py
+++ testStats.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-df = pd.read_csv (r'makemoons_3_10_10_3_.csv')
+df = pd.read_csv (r'layers_to_csv_2_3_1_.csv')
 print("\nMoyenne du résultat de la fonction d'activation pour chaque neurones sans prise en compte classes : ")
 print(df.mean(axis = 0)[1:len(df)])   
 
@@ -24,5 +24,16 @@ data = pd.DataFrame({
     "Histogramme":df.iloc[:,0]
     })
 hist = data.hist(bins=2)
-
 df.plot.box()
+
+
+dataN2 = pd.DataFrame({
+        "Histogramme N2":df.iloc[:,3]
+        })
+hist = dataN2.hist(bins=3)
+df.plot.box()
+
+
+df["valQ"]=pd.qcut(df["N2"],2,labels=["0","1"])
+df["valQ"].describe()
+print("test :",df["valQ"])
